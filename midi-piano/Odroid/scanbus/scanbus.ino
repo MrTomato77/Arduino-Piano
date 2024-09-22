869e1b76790a14c3975a0d682cff517c80fc5cfd
ARTTTT_TTTT <tanapath.tp@gmail.com>
2024-09-22 17:56:23 +0700

    update scanbus

--- midi-piano/Odroid/scanbus/scanbus.ino
+++ midi-piano/Odroid/scanbus/scanbus.ino
@@ -1,38 +1,47 @@
-// I2C Scanner
-// Written by Nick Gammon
-// Date: 20th April 2011
-
 #include <Wire.h>
 
 void setup() {
-    Serial.begin(115200);
+  Wire.begin(); // Start the I2C bus
+  Serial.begin(9600); // Start the Serial communication at 9600 baud rate
+  while (!Serial); // Wait for Serial to initialize
+  Serial.println("I2C Scanner Ready. Scanning...");
+}
 
-    // Leonardo: wait for serial port to connect
-    while (!Serial) {
+void loop() {
+  byte error, address;
+  int nDevices = 0;
+
+  Serial.println("Scanning...");
+
+  for (address = 1; address < 127; address++ ) {
+    // Start the I2C transmission
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    // Check for device presence
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.print(address, HEX);
+      Serial.println("  !");
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
     }
+  }
 
-    Serial.println();
-    Serial.println("I2C scanner. Scanning ...");
-    byte count = 0;
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found.\n");
+  } else {
+    Serial.print(nDevices);
+    Serial.println(" device(s) found.\n");
+  }
 
-    Wire.begin();
-    for (byte i = 8; i < 120; i++) {
-        Wire.beginTransmission(i);
-        if (Wire.endTransmission() == 0) {
-            Serial.print("Found address: ");
-            Serial.print(i, DEC);
-            Serial.print(" (0x");
-            Serial.print(i, HEX);
-            Serial.println(")");
-            count++;
-            delay(1); // maybe unneeded?
-        } // end of good response
-    } // end of for loop
-
-    Serial.println("Done.");
-    Serial.print("Found ");
-    Serial.print(count, DEC);
-    Serial.println(" device(s).");
-} // end of setup
-
-void loop() {}
+  delay(5000); // Wait 5 seconds before the next scan
+}
