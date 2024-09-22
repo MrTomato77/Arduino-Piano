5b2d9b41fc6af1b785b626001fdcc2228dc6324a
ARTTTT_TTTT <tanapath.tp@gmail.com>
2024-09-22 18:35:49 +0700

    update test from github

--- midi-piano/Odroid/test.py
+++ midi-piano/Odroid/test.py
@@ -1,17 +1,10 @@
-from luma.core.interface.serial import i2c
-from luma.lcd import CharLCD
 import time
 
-serial = i2c(port=1, address=0x27)
-lcd = CharLCD(serial, width=16, height=2)
+from luma.core.interface.serial import pcf8574
+from luma.lcd.device import hd44780
 
-lcd.clear()
-lcd.text('hola', 0, 0)
+interface = pcf8574(address=0x27, backlight_enabled=True)
+device = hd44780(interface, width=16, height=2)
+device.text = "Hello world"
 
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
-finally:
-    lcd.clear()
+time.sleep(20000)
