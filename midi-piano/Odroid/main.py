0b5c6b98be1550af923588cb7b9b5744acb91c20
ARTTTT_TTTT <tanapath.tp@gmail.com>
2024-09-25 15:13:14 +0700

    update uart connection

--- midi-piano/Odroid/main.py
+++ midi-piano/Odroid/main.py
@@ -1,16 +1,20 @@
-# main.py
 from uart import setup_uart, read_bpm
 from metronome import setup_buzzer, run_metronome
 
 def main():
+    setup_buzzer(buzzer_pin)
     uart = setup_uart()
     buzzer_pin = 0
-
-    setup_buzzer(buzzer_pin)
+    prev_bpm = None
 
     while True:
         bpm = read_bpm(uart)
-        run_metronome(bpm, buzzer_pin)
+        if bpm != prev_bpm:
+            print(f"BPM changed from {prev_bpm} to {bpm}. Restarting metronome.")
+            prev_bpm = bpm
+            run_metronome(bpm, buzzer_pin)
+        else:
+            run_metronome(bpm, buzzer_pin)
 
 if __name__ == "__main__":
     main()
